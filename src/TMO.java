c7f81cffd6a015e55a62fb0adf21fa1e7421cfa7
Ilya <lyah.ilya88@gmail.com>
2019-12-21 15:57:25 +0300

    -fixed mulMatrix and print Methods -int's replaced with double's -added all methods to readme.md

--- src/TMO.java
+++ src/TMO.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class TMO {
-    private static int[][] E;
-    private static Vector<int[][]> Qi;
-    private static Vector<int[][]> Fi;
-    private static void print(int[][] matrix) {
-        for (int i = 0; i < matrix[0].length; ++i) {
-            for (int j = 0; j < matrix.length; ++j) {
-                System.out.print(matrix[i][j] + " ");
+    private static double[][] E;
+    private static Vector<double[][]> Qi;
+    private static Vector<double[][]> Fi;
+
+    private static void print(double[][] matrix) {
+        for (double[] row : matrix) {
+            for (double elem : row) {
+                System.out.print(elem + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    private static int[][] Gaus(int[][] A, int[][] b) { //решение системы линейных уравнение методом Гаусса
-
-        int[][] x = new int[1][A.length];
-        int[][] B = new int[A.length][A[0].length];
+    private static double[][] Gaus(double[][] A, double[][] b) { //решение системы линейных уравнение методом Гаусса
+        double[][] x = new double[1][A.length];
+        double[][] B = new double[A.length][A[0].length];
         for (int i = 0; i < B.length; i++) {
             for (int j = 0; j < B[0].length; j++) {
                 B[i][j] = A[i][j];
@@ -52,8 +52,8 @@ public class TMO {
         return x;
     }
 
-    public static int[][] mulMatr(int[][] A, int val) {
-        int[][] res = new int[A.length][A[0].length];
+    private static double[][] mulMatr(double[][] A, int val) {
+        double[][] res = new double[A.length][A[0].length];
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A.length; j++) {
                 res[i][j] = A[i][j] * val;
@@ -62,11 +62,11 @@ public class TMO {
         return res;
     }
 
-    public static int[][] mulMatrix(int[][] A, int[][] B) {
-        int[][] res = new int[A.length][B.length];
+    private static double[][] mulMatrix(double[][] A, double[][] B) {
+        double[][] res = new double[A.length][B[0].length];
         for (int i = 0; i < res.length; i++) {
             for (int j = 0; j < res[0].length; j++) {
-                for (int k = 0; k < res[0].length; k++) {
+                for (int k = 0; k < B.length; k++) {
                     res[i][j] += A[i][k] * B[k][j];
                 }
             }
@@ -74,11 +74,11 @@ public class TMO {
         return res;
     }
 
-    static public int[][] InverseMatr(int[][] A) { //нахождение обратной матрицы
-        int[][] inverse = new int[A.length][A[0].length];
+    private static double[][] InverseMatr(double[][] A) { //нахождение обратной матрицы
+        double[][] inverse = new double[A.length][A[0].length];
 
         for (int i = 0; i < A.length; i++) {
-            int[][] b = new int[A.length][1];
+            double[][] b = new double[A.length][1];
             b[i][0] = 1;
             inverse[i] = Gaus(A, b)[0];
         }
@@ -87,8 +87,8 @@ public class TMO {
         return inverse;
     }
 
-    private static int[][] TransposeMatr(int[][] A) {
-        int[][] res = new int[A.length][A[0].length];
+    private static double[][] TransposeMatr(double[][] A) {
+        double[][] res = new double[A.length][A[0].length];
         for (int i = 0; i < res.length; i++) {
             for (int j = 0; j < res[0].length; j++) {
                 res[i][j] = A[j][i];
@@ -97,8 +97,8 @@ public class TMO {
         return res;
     }
 
-    private static int[][] mulVect(int[] a, int[] b) {
-        int[][] res = new int[a.length][b.length];
+    private static double[][] mulVect(double[] a, double[] b) {
+        double[][] res = new double[a.length][b.length];
         for (int i = 0; i < res.length; i++) {
             for (int j = 0; j < res[0].length; j++) {
                 res[i][j] = a[i] * b[j];
@@ -107,8 +107,8 @@ public class TMO {
         return res;
     }
 
-    private static int[] matrXColumn(int[][] d, int[] x1) {
-        int[] r = new int[d.length];
+    private static double[] matrXColumn(double[][] d, double[] x1) {
+        double[] r = new double[d.length];
         for (int i = 0; i < d.length; i++) {
             for (int j = 0; j < d.length; j++) {
                 r[i] += d[i][j] * x1[j];
@@ -117,18 +117,18 @@ public class TMO {
         return r;
     }
 
-    private static int[][] readMatrix(Scanner sc, int n) {
-        int[][] res = new int[n][n];
+    private static double[][] readMatrix(Scanner sc, int n) {
+        double[][] res = new double[n][n];
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                res[i][j] = sc.nextInt();
+                res[i][j] = sc.nextDouble();
             }
         }
         return res;
     }
 
-    private static int[][] KronMult(int[][] A, int[][] B) { //кронекерово произведение матриц
-        int[][] K = new int[B.length * A.length][B[0].length * A[0].length];
+    private static double[][] KronMult(double[][] A, double[][] B) { //кронекерово произведение матриц
+        double[][] K = new double[B.length * A.length][B[0].length * A[0].length];
 
         for (int i = 0; i < A.length; i++)
             for (int j = 0; j < A[0].length; j++)
@@ -140,8 +140,8 @@ public class TMO {
         return K;
     }
 
-    private static int[][] copyMatrix(int[][] A) {
-        int[][] res = new int[A.length][A[0].length];
+    private static double[][] copyMatrix(double[][] A) {
+        double[][] res = new double[A.length][A[0].length];
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[0].length; j++) {
                 res[i][j] = A[i][j];
@@ -150,22 +150,23 @@ public class TMO {
         return res;
     }
 
-    private static int[][] KronSum(int[][] A, int[][] B) {
-        int[][] Ia = new int[B.length][B[0].length];
-        int[][] Ib = new int[A.length][A[0].length];
+    private static double[][] KronSum(double[][] A, double[][] B) {
+        double[][] Ia = new double[B.length][B[0].length];
+        double[][] Ib = new double[A.length][A[0].length];
         for (int i = 0; i < A.length; i++) {
             Ia[i][i] = Ib[i][i] = 1;
         }
         return Sum(KronMult(A, Ib), KronMult(B, Ia));
     }
+
     //    private static int[][] countF(int index){
 //        if(index == 0){
 //            return E;
 //        }
 //        int[][] res = mulMatrix(countF(index-1),);
 //    }
-    private static int[][] Sum(int[][] A, int[][] B) { //сумма матриц
-        int[][] K = new int[A[0].length][A.length];
+    private static double[][] Sum(double[][] A, double[][] B) { //сумма матриц
+        double[][] K = new double[A[0].length][A.length];
         for (int i = 0; i < A[0].length; i++) {
             for (int j = 0; j < A.length; j++) {
                 K[i][j] = B[i][j] + A[i][j];
@@ -174,11 +175,11 @@ public class TMO {
         return K;
     }
 
-    public static double norm(int[][] matrix) {
+    private static double norm(double[][] matrix) {
         double norm = 0;
-        for (int[] row : matrix) {
+        for (double[] row : matrix) {
             double summ = 0;
-            for (int element : row)
+            for (double element : row)
                 summ += Math.abs(element);
             if (summ > norm)
                 norm = summ;
@@ -192,62 +193,59 @@ public class TMO {
         Qi = new Vector<>();
         Fi = new Vector<>();
         int n = W + 1;
-        int[][] D0 = readMatrix(sin, n);
-        int[][] D1 = readMatrix(sin, n);
+        double[][] D0 = readMatrix(sin, n);
+        double[][] D1 = readMatrix(sin, n);
         int M = sin.nextInt();
-        int[] beta = new int[n];
-        int[][] betaM = new int[n][1];
+        double[] beta = new double[n];
+        double[][] betaM = new double[n][1];
         for (int i = 0; i < n; i++) {
-            beta[i] = betaM[i][0] = sin.nextInt();
+            beta[i] = betaM[i][0] = sin.nextDouble();
 
         }
-        int[][] S = readMatrix(sin, n);
+        double[][] S = readMatrix(sin, n);
         double eps = sin.nextDouble();
-        E = new int[n][n];
-        int[][] Im = new int[M][M];
+        E = new double[n][n];
+        double[][] Im = new double[M][M];
         for (int i = 0; i < n; ++i) {
             E[i][i] = 1;
             Im[i][i] = 1;
         }
-        int[] e = new int[n];
+        double[] e = new double[n];
         for (int i = 0; i < n; i++) {
             e[i] = -1;
         }
-        int[] S0 = matrXColumn(S, e);
-        int[][] S0M = new int[n][1];
+        double[] S0 = matrXColumn(S, e);
+        double[][] S0M = new double[n][1];
         for (int i = 0; i < n; i++) {
             S0M[i][0] = S0[i];
         }
-        int[][] Q0 = KronMult(E, mulVect(S0, beta));
-        int[][] Q1 = KronSum(D0, S);
-        int[][] Q2 = KronMult(D1, Im);
-        int[][] Q01 = KronMult(D1, betaM);
-        int[][] Q10 = KronMult(E, S0M);
-        int[][] GPrev = new int[n * M][n * M];
-        print(Q0);
-        print(Q1);
-        print(Q2);
-        int[][] GNext = new int[n * M][n * M];
-        double normG = 0;
+        double[][] Q0 = KronMult(E, mulVect(S0, beta));
+        double[][] Q1 = KronSum(D0, S);
+        double[][] Q2 = KronMult(D1, Im);
+        double[][] Q01 = KronMult(D1, betaM);
+        double[][] Q10 = KronMult(E, S0M);
+        double[][] GPrev = new double[n * M][n * M];
+        print(Q01);
+        double[][] GNext;
+        double normG;
         do {
             GNext = mulMatrix(InverseMatr(mulMatr(Q1, -1)), (Sum(Q0, mulMatrix(Q2, mulMatrix(GPrev, GPrev)))));
             normG = norm(Sum(GNext, mulMatr(GPrev, -1)));
             GPrev = copyMatrix(GNext);
         } while (normG < eps);
-        int[][] G0 = mulMatrix(mulMatr(InverseMatr(Sum(Q1, mulMatrix(Q2, GPrev))), -1), Q10);
+        double[][] G0 = mulMatrix(mulMatr(InverseMatr(Sum(Q1, mulMatrix(Q2, GPrev))), -1), Q10);
         double normF = norm(E);
-        for(int i = 1;normF>=eps ;i++){
-            if(i == 0){
-                Qi.add(Sum(Q0,mulMatrix(Q01,G0)));
-                Qi.add(Q01);
-            }
-            else{
-                Qi.add(Sum(Q1,mulMatrix(Q2,GPrev)));
-                Qi.add(Q2);
-            }
-            // int[][] tmp = countF(i);
-            // normF=norm(tmp);
-            // Fi.add(tmp);
-        }
+//        for (int i = 1; normF >= eps; i++) {
+//            if (i == 0) {
+//                Qi.add(Sum(Q0, mulMatrix(Q01, G0)));
+//                Qi.add(Q01);
+//            } else {
+//                Qi.add(Sum(Q1, mulMatrix(Q2, GPrev)));
+//                Qi.add(Q2);
+//            }
+        // int[][] tmp = countF(i);
+        // normF=norm(tmp);
+        // Fi.add(tmp);
+        //       }
     }
 }
